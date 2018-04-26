09c838d96d66e9947b54d59be2c4926b566152a7
TopShares <1255877369@qq.com>
2018-04-26 15:02:13 +0800

    m

---
+++ test.js
@@ -0,0 +1,1 @@
+var a = "[{"id":"DemandNumber","comparisonChar":"eq","comparisonValue":"2","comparisonType":"text"},{"id":"DemandNumber","comparisonChar":"eq","comparisonValue":"3","comparisonType":"text"},{"id":"DemandNumber","comparisonChar":"eq","comparisonValue":"1","comparisonType":"text"}]"
